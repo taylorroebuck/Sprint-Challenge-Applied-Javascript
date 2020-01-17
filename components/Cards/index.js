5de7c128bfe94abb6aedb6dc3880371f1a36db4f
TN Roebuck <tnroebuck@TNs-MacBook-Pro.local>
2020-01-17 11:47:35 -0700

    finished MVP goals

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,16 +18,32 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
+const cards = document.querySelector('.cards-container');
+
 axios
 .get('https://lambda-times-backend.herokuapp.com/articles')
 .then(response => {
     console.log(response);
+
+    response.data.articles.javascript.forEach(article => {
+        cards.append(createCard(article));
     })
+    response.data.articles.bootstrap.forEach(article => {
+        cards.append(createCard(article));
+    })
+    response.data.articles.technology.forEach(article => {
+        cards.append(createCard(article));
+    })
+    response.data.articles.jquery.forEach(article => {
+        cards.append(createCard(article));
+    });
+    response.data.articles.node.forEach(article => {
+        cards.append(createCard(article));
+    });
+})
 .catch(error => {
     console.log(error);
 })
-
-const cards = document.querySelector('.cards-container');
 
 //create function
 function createCard(article) {
@@ -35,8 +51,27 @@ function createCard(article) {
     const headline = document.createElement('div');
     const author = document.createElement('div');
     const imageContainer = document.createElement('div');
-        const image = document.createElement('img');
+    const image = document.createElement('img');
     const byline = document.createElement('span');
 
+    //setup structure of elements
+    card.append(headline);
+    card.append(author);
+    author.append(imageContainer);
+    imageContainer.append(image);
+    author.append(byline);
+
+    //add classes to elements
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imageContainer.classList.add('img-container');
+
+    //set content
+    headline.textContent = article.headline;
+    image.src = article.authorPhoto;
+    byline.textContent = `By ${article.authorName}`;
+
+    return card;
 }
 
